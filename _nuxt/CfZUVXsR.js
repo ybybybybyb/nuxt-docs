08f771c76d92d69a911f59ec03c2dcb43a31ee0c
ybybybybyb <35048800+ybybybybyb@users.noreply.github.com>
2025-10-30 20:50:16 +0000

    Deploying to gh-pages from @ ybybybybyb/nuxt-docs@e7a63633bfd5d5de89a91ce676b205a69d08e629 🚀

---
+++ _nuxt/CfZUVXsR.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CtrXBFeh.js";import a from"./D2YGUxzL.js";import{z as s,H as t,i as e,w as i,W as p}from"./Dl-R3BBK.js";import"./BN_7HF1G.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
